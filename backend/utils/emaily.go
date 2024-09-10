f8d85d42aefafcf4d164b7c4c34b951f8abd49f0
Firu115 <firu@post.cz>
2024-09-10 23:38:55 +0200

    vylepšení e-mailů
    brutální peklo, cítím se jak při začátcích internetu (╥﹏╥)

--- backend/utils/emaily.go
+++ backend/utils/emaily.go
@@ -1,13 +1,19 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"text/template"
 
 	"gopkg.in/gomail.v2"
 )
+
+type fill struct {
+	Kod string
+}
 
 // pošle email s kódem přes emailového poskytovatele
 func PoslatOverovaciEmail(email string, kod string) error {
@@ -16,16 +22,30 @@ func PoslatOverovaciEmail(email string, kod string) error {
 		log.Panic("konverze portu na int se rozbila")
 	}
 
+	t, err := template.ParseFiles("./utils/overovaci_email.html")
+	if err != nil {
+		MobilNotifikace("NEFUNGUJE MAIL " + err.Error())
+	}
+	data := fill{
+		Kod: kod,
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return err
+	}
+
 	m := gomail.NewMessage()
 	m.SetHeader("From", fmt.Sprintf("Jako Pavouk <%v>", os.Getenv("EMAIL_FROM")))
 	m.SetHeader("To", email)
 	m.SetHeader("Subject", "Verifikace")
-	m.SetBody("text/html", fmt.Sprintf("Váš ověřovací kód na Jako Pavouk je: <b>%v</b>", kod))
+	m.AddAlternative("text/plain", fmt.Sprintf("Tvůj ověřovací kód je: %s", kod))
+	m.SetBody("text/html", buf.String())
+	m.Embed("./utils/pavoucekDoEmailu.png")
 
 	d := gomail.NewDialer(os.Getenv("EMAIL_HOST"), port, os.Getenv("EMAIL_FROM"), os.Getenv("EMAIL_HESLO"))
 	if err := d.DialAndSend(m); err != nil {
 		log.Print("NEFUNGUJE MAIL GG WOOHOO", err)
-		MobilNotifikace("NEFUNGUJE MAIL")
+		MobilNotifikace("NEFUNGUJE MAIL " + err.Error())
 		return err
 	}
 	log.Println("Posláno -", email)
